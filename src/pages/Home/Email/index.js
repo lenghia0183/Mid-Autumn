0b3ad36f4e7077dbdaee96cf005a2a2c2e171823
Lenghia0183 <lenghia0183@gmail.com>
2024-09-29 01:12:55 +0700

    update responsive home page

--- src/pages/Home/Email/index.js
+++ src/pages/Home/Email/index.js
@@ -14,7 +14,7 @@ function Email() {
           backgroundRepeat: "no-repeat",
           backgroundAttachment: "fixed",
         }}
-        className="py-[100px] flex items-center justify-center mt-14 relative"
+        className="xl:py-[100px] sm:py-[80px] py-[60px] flex items-center justify-center mt-14 relative"
       >
         <div
           style={{
@@ -23,17 +23,17 @@ function Email() {
           className="h-full w-full absolute inset-0 pointer-events-none"
         ></div>
         <div className="text-center p-10 z-[1000] w-full">
-          <p className="text-xl text-yellow font-semibold">
+          <p className="sm:text-2xl text-xl text-yellow font-semibold">
             {t("common.midAutumnFestival")}
           </p>
-          <h2 className="text-[50px] font-semibold text-white-100 mt-3">
+          <h2 className="xl:text-[50px] sm:text-[40px] text-3xl font-semibold text-white-100 mt-2">
             {t("home.email.title")}
           </h2>
 
           <div className="container">
-            <div className="relative w-[70%] m-auto">
+            <div className="relative sm:block flex flex-col xl:w-[70%] m-auto xl:mt-10 sm:mt-5 mt-4">
               <input
-                className="absolute inset-0 z-0 outline-none rounded-full p-5"
+                className="sm:absolute inset-0 z-0 outline-none rounded-full p-5"
                 type="email"
                 placeholder={t("home.email.inputPlaceholder")}
               ></input>
@@ -45,7 +45,7 @@ function Email() {
                 size="large"
                 width="150px"
                 height="60px"
-                className="ml-auto hover:text-dark font-semibold px-10 mt-5 z-[1000] relative"
+                className="sm:ml-auto sm:mr-0 ml-auto mr-auto hover:text-dark font-semibold px-10 mt-5 z-[1000] relative"
               >
                 {t("common.register")}
               </Button>
