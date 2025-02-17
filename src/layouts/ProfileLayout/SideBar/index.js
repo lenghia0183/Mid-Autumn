8b1e0ba0ddb9b518138fb80cfd1b27f78b422a6d
Lenghia0183 <lenghia0183@gmail.com>
2025-02-18 00:14:30 +0700

    Update multi language for side bar

--- src/layouts/ProfileLayout/SideBar/index.js
+++ src/layouts/ProfileLayout/SideBar/index.js
@@ -1,31 +1,45 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import Icon from "../../../components/Icon";
-import { PATH } from "../../../constants/path";
+import { PAGE_TITLE, PATH } from "../../../constants/path";
 import Button from "../../../components/Button";
 
 import { useUser } from "../../../context";
 import { triggerLogout } from "../../../utils";
+import { useTranslation } from "react-i18next";
 
 function SideBar() {
-  const { logout } = useUser();
+  const { logout, user: userData } = useUser();
+  console.log("user", userData.user);
   const navigate = useNavigate();
 
+  const { t } = useTranslation();
+
   const accountLinks = [
-    { name: "Thông tin tài khoản", path: PATH.PROFILE_EDIT, icon: "edit" },
-    { name: "Đổi mật khẩu", path: PATH.CHANGE_PASSWORD, icon: "key" },
+    { name: t(PAGE_TITLE.PROFILE), path: PATH.PROFILE_EDIT, icon: "edit" },
+    {
+      name: t(PAGE_TITLE.CHANGE_PASSWORD),
+      path: t(PATH.CHANGE_PASSWORD),
+      icon: "key",
+    },
   ];
 
   const transactionLinks = [
-    { name: "Đơn hàng của bạn", path: PATH.ORDER, icon: "order" },
-    { name: "Sản phẩm đã xem", path: PATH.VIEWED_PRODUCTS, icon: "eye" },
-    { name: "Danh sách yêu thích", path: PATH.FAVORITE, icon: "heart" },
+    { name: t(PAGE_TITLE.ORDER), path: PATH.ORDER, icon: "order" },
+    {
+      name: t(PAGE_TITLE.VIEWED_PRODUCTS),
+      path: t(PATH.VIEWED_PRODUCTS),
+      icon: "eye",
+    },
+    { name: t(PAGE_TITLE.FAVORITE), path: PATH.FAVORITE, icon: "heart" },
   ];
 
   return (
     <nav className="p-4 sm:pb-[150px] pb-[100px] bg-white-100 border-r border-gray-200">
       <div className="mb-4">
-        <div className="text-dark text-xl font-semibold">LECONGNGHIA</div>
-        <div className="text-gray-400 text-lg ">Chỉnh sửa tài khoản</div>
+        <div className="text-dark text-xl font-semibold">
+          {userData?.user?.fullname}
+        </div>
+        <div className="text-gray-400 text-lg ">{userData?.user?.email}</div>
       </div>
 
       <div className="mb-6">
@@ -89,7 +103,7 @@ function SideBar() {
           logout(navigate);
         }}
       >
-        Đăng xuất
+        {t("common.logout")}
       </Button>
     </nav>
   );
